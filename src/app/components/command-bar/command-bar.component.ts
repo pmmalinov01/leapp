2b1f1b0ec6542b8186ce99c112889ac58e1107a0
Riccardo Rotta <riccardo.rotta@besharp.it>
2021-12-01 17:51:50 +0100

    chore: created create-dialog.component modal

--- src/app/components/command-bar/command-bar.component.ts
+++ src/app/components/command-bar/command-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {BsModalService} from 'ngx-bootstrap/modal';
 import {OptionsDialogComponent} from '../dialogs/options-dialog/options-dialog.component';
+import {CreateDialogComponent} from "../dialogs/create-dialog/create-dialog.component";
 
 @Component({
   selector: 'app-command-bar',
@@ -15,6 +16,10 @@ export class CommandBarComponent implements OnInit {
   }
 
   showOptionDialog() {
-    this.bsModalService.show(OptionsDialogComponent);
+    this.bsModalService.show(OptionsDialogComponent, { animated: false, class: 'confirm-modal'});
+  }
+
+  showCreateDialog() {
+    this.bsModalService.show(CreateDialogComponent, { animated: false, class: 'confirm-modal'});
   }
 }
